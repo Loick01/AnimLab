b28579216a48506d09b3ea9e510e603408ad1879
Loick01 <loicker89@gmail.com>
2026-03-27 22:46:51 +0100

    Add 3d scene with shadow map

--- include/type.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <cmath>
-
-inline float radians(const float degrees) { // Clockwise
-    return degrees * M_PI/180;
-}
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "application.hpp"
-
-int main() {
-    Application app("AnimLab", sf::Color(33,33,33));
-    app.Run();
-    return 0;
-}
